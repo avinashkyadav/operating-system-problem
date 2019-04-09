06a2ecd8d014883b57b92785380fe3c00266d28e
Avinash Kumar Yadav <43061158+avinashkyadav@users.noreply.github.com>
2019-04-09 04:00:09 +0000

    Add files via upload

---
+++ os_project1.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+class input_local{
+protected:
+ int 	no_of_processes, total, random1, counter, time_quantum_p,counter1; 
+      int wait_time_p, turnaround_time_p,qq,z,p[20],prio[20], a_time[20], b_time[20], temp[20],b,local_var; 
+float average_wait_time_P, average_turnaround_time_p;  
+
+
+input_local(){
+int 	no_of_processes=0, total=0, random1=0, counter=0, time_quantum_p=0,counter1=0;
+float average_wait_time_P=0, average_turnaround_time_p=0;
+
+}};
+
+int main(){
+return 0;
+
+
+}
